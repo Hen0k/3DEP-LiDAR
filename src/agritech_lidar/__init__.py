8d8894b68830971db3e968cfa63dd2b9eeafcd7a
Hen0k <henoktilaye7@gmail.com>
2022-06-26 12:54:27 +0300

    doc: import readme content as package doc

--- src/agritech_lidar/__init__.py
+++ src/agritech_lidar/__init__.py
@@ -1,7 +1,5 @@
 """
-# agritech lidar
 
-This package is an a primerily data accessing tool for lidar point cloud data that is stored in the [entwine](https://entwine.io/entwine-point-tile.html) format.
+.. include:: ../../README.md
 
-In future Iterations, It will include data visualization and more features extractions tools.
 """
